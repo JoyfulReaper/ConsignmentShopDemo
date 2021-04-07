71c8096b89644d90145ee09e51e16ae03b4ecc83
Kyle Givler <xvegan88x@gmail.com>
2021-04-07 18:26:01 -0400

    ItemData tests

--- ConsignmentShopTests/SQLiteItemDataTests.cs
+++ ConsignmentShopTests/SQLiteItemDataTests.cs
@@ -25,6 +25,7 @@ SOFTWARE.
 
 using ConsignmentShopLibrary.Data.SQLite;
 using ConsignmentShopLibrary.Models;
+using System.Linq;
 using System.Text;
 using Xunit;
 
@@ -64,6 +65,158 @@ namespace ConsignmentShopTests
             Assert.Equal("Create Description", dbItem.Description);
             Assert.False(dbItem.PaymentDistributed);
             Assert.Equal(_vendor.Id, dbItem.OwnerId);
+        }
+
+        [Fact]
+        public async void Test_LoadItem()
+        {
+            var dbItem = await _itemData.LoadItem(1);
+            Assert.NotNull(dbItem);
+
+            Assert.Equal("Test Item", dbItem.Name);
+            Assert.Equal("Test Description", dbItem.Description);
+            Assert.False(dbItem.PaymentDistributed);
+            Assert.Equal(_vendor.Id, dbItem.OwnerId);
+        }
+
+        [Fact]
+        public async void Test_LoadAllItems()
+        {
+            //TODO Improve test
+            var allItems = await _itemData.LoadAllItems();
+            Assert.NotNull(allItems);
+            Assert.True(allItems.Count > 0);
+        }
+
+        [Fact]
+        public async void Test_LoadItemsByVendor()
+        {
+            //TODO Improve test
+            var allItems = await _itemData.LoadItemsByVendor(_vendor);
+            Assert.NotNull(allItems);
+            Assert.True(allItems.Count > 0);
+            Assert.True(allItems.TrueForAll(x => x.OwnerId == _vendor.Id));
+        }
+
+        [Fact]
+        public async void Test_LoadSoldItems()
+        {
+            var item = new ItemModel()
+            {
+                Name = "Sold Item",
+                Description = "Sold Description",
+                Price = 1.00m,
+                Sold = true,
+                PaymentDistributed = false,
+                OwnerId = _vendor.Id,
+                Owner = _vendor,
+            };
+
+            int id = await _itemData.CreateItem(item);
+
+            var allItems = await _itemData.LoadSoldItems();
+            Assert.NotNull(allItems);
+            Assert.True(allItems.Count > 0);
+            Assert.True(allItems.TrueForAll(x => x.Sold == true));
+
+            item = allItems.Where(x => x.Id == id).FirstOrDefault();
+            Assert.NotNull(item);
+            Assert.Equal("Sold Item", item.Name);
+            Assert.Equal("Sold Description", item.Description);
+            Assert.False(item.PaymentDistributed);
+            Assert.Equal(_vendor.Id, item.OwnerId);
+        }
+
+        [Fact]
+        public async void Test_LoadSoldItemsByVendor()
+        {
+            var item = new ItemModel()
+            {
+                Name = "Vendor Sold Item",
+                Description = "Vendor Sold Description",
+                Price = 1.00m,
+                Sold = true,
+                PaymentDistributed = false,
+                OwnerId = _vendor.Id,
+                Owner = _vendor,
+            };
+
+            int id = await _itemData.CreateItem(item);
+
+            var allItems = await _itemData.LoadSoldItems();
+            Assert.NotNull(allItems);
+            Assert.True(allItems.Count > 0);
+            Assert.True(allItems.TrueForAll(x => x.Sold == true));
+            Assert.True(allItems.TrueForAll(x => x.OwnerId == _vendor.Id));
+
+            item = allItems.Where(x => x.Id == id).FirstOrDefault();
+            Assert.NotNull(item);
+            Assert.Equal("Vendor Sold Item", item.Name);
+            Assert.Equal("Vendor Sold Description", item.Description);
+            Assert.False(item.PaymentDistributed);
+            Assert.Equal(_vendor.Id, item.OwnerId);
+        }
+
+        [Fact]
+        public async void Test_LoadUnsoldItems()
+        {
+            var allItems = await _itemData.LoadUnsoldItems();
+            Assert.NotNull(allItems);
+            Assert.True(allItems.Count > 0);
+            Assert.True(allItems.TrueForAll(x => x.Sold == false));
+        }
+
+        [Fact]
+        public async void Test_RemoveItem()
+        {
+            var item = new ItemModel()
+            {
+                Name = "Delete Item",
+                Description = "Delete Description",
+                Price = 1.00m,
+                Sold = true,
+                PaymentDistributed = true,
+                OwnerId = _vendor.Id,
+                Owner = _vendor,
+            };
+
+            int id = await _itemData.CreateItem(item);
+
+            await _itemData.RemoveItem(item);
+
+            item = await _itemData.LoadItem(id);
+            Assert.Null(item);
+        }
+
+        [Fact]
+        public async void Test_UpdateItem()
+        {
+            var item = new ItemModel()
+            {
+                Name = "Update Item",
+                Description = "Update Description",
+                Price = 1.00m,
+                Sold = false,
+                PaymentDistributed = false,
+                OwnerId = _vendor.Id,
+                Owner = _vendor,
+            };
+
+            int id = await _itemData.CreateItem(item);
+
+            item.Name = "Updated Item";
+            item.Price = 5.00m;
+            item.Sold = true;
+
+            await _itemData.UpdateItem(item);
+
+            var dbItem = _itemData.LoadItem(id);
+            Assert.NotNull(item);
+            Assert.Equal("Updated Item", item.Name);
+            Assert.Equal("Update Description", item.Description);
+            Assert.Equal(5.00m, item.Price);
+            Assert.False(item.PaymentDistributed);
+            Assert.Equal(_vendor.Id, item.OwnerId);
         }
 
         protected override async void Seed()
